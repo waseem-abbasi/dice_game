09fa90ac35fef9c05145d2ce2d257f3654cab8e6
waseem-abbasi <waseemabbasi5111@gmail.com>
2026-04-17 10:22:41 +0530

    changes in startgame compnent center the button and remove the border

--- src/componenets/StartGame.jsx
+++ src/componenets/StartGame.jsx
@@ -6,15 +6,17 @@ const StartGame = ({toggle}) => {
         <>
             <Container>
                 <Row className="min-vh-100 d-flex justify-content-center align-items-center">
-                    <Col className="border" md={6}>
+                    <Col className="" md={6}>
                         <div className="d-flex align-items-stretch">
                             <img src="./images/dice.png" alt="" className="img-fluid" />
                         </div>
                     </Col>
-                    <Col className="border" md={6}>
+                    <Col className="" md={6}>
                         <h1 style={{ fontSize: "100px" }}>DICE GAME</h1>
-                        <div className="text-end me-4">
-                            <Button variant="dark" onClick={toggle}>start</Button>
+                        <div className="text-center me-4">
+                           
+                            <Button variant="outline-dark" onClick={toggle} className="text-center ">start</Button>
+                          
                         </div>
                     </Col>
                 </Row>
